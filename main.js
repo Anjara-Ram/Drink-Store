5abbfd9087ca67df7bd723588054a9bad17bf549
Anjara-Ram <anjaraven159@gmail.com>
2025-02-14 23:18:12 +0300

    Home setion

---
+++ main.js
@@ -0,0 +1,9 @@
+// Swiper 
+var swiper = new Swiper(".swiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
